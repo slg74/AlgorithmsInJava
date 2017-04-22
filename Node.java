a74f9961d3985e4b1ebcfb1165dac73c9bbb7375
U-MSI\scott <scott.layne.gillespie@gmail.com>
2017-04-22 00:32:36 -0500

    in order traversal and insertion of an element into a binary treeB

---
+++ Node.java
@@ -0,0 +1,69 @@
+class Node {
+	Node left, right;
+	int data;
+	public Node(int data) {	
+		this.data = data; 
+	}
+
+	public void insert(int value) {
+		if (value <= data) {		
+			if (left == null) {
+				left = new Node(value);
+			} else {
+				left.insert(value);
+			}
+		} else {
+			if (right == null) {
+				right = new Node(value);
+			} else {
+				right.insert(value); 
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Node a = new Node(5); 
+		Node b = new Node(10); 
+		Node c = new Node(15); 
+		Node d = new Node(8); 
+
+		b.left = a;
+		b.right = c;
+
+		printInOrder(b); 
+
+		System.out.println("After inserting 8..."); 
+		b.insert(8); 
+		printInOrder(b); 
+	}
+
+	public boolean contains(int value) {
+		if (value == data) {
+			return true;
+		} else if (value < data) {	
+			if (left == null) {
+				return false;
+			} else {
+				return left.contains(value); 
+			}
+		} else {
+			if (right == null) {
+				return false;
+			} else {
+				return right.contains(value);
+			}
+		}
+	}
+
+	public static void printInOrder(Node n) {
+		if (n.left != null) {
+			n.left.printInOrder(n.left); 
+		}
+		System.out.println(n.data);
+		if (n.right != null) {
+			n.right.printInOrder(n.right); 
+		}
+	}
+}
+	
